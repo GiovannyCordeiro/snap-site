4c09d19bbfb7c91757747ff8886b58f9a4163702
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2022-07-13 22:16:31 -0300

    BlackWindowHeader

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
 const menuHamburguer = document.getElementById('menuHamburguer');
 const closeMenu = document.getElementById('closeMenu');
 const links = document.getElementById('links');
+const blackWindow = document.getElementById('blackwindow');
 
 function activeMenuMobile(){
     links.classList.add('active');
+    blackWindow.classList.add('active');
 }
 
 function removeMenuMobile(){
     links.classList.remove('active');
+    blackWindow.classList.remove('active');
 }
 
-menuHamburguer.addEventListener('click',activeMenuMobile)
-closeMenu.addEventListener('click',removeMenuMobile)
+menuHamburguer.addEventListener('click', activeMenuMobile)
+closeMenu.addEventListener('click', removeMenuMobile)
+blackWindow.addEventListener('click', removeMenuMobile)
